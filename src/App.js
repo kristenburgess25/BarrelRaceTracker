2bfba349bdafa20a86d88757ee4bffb2ec25d6ae
Kristen Burgess <kristen.burgess@doane.edu>
2016-12-26 19:08:18 -0700

    Successfully hooked up to firebase

--- src/App.js
+++ src/App.js
@@ -2,8 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import EventList from './EventList';
+import { pick, map, extend } from 'lodash';
+import firebase, { reference } from './firebase';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      barrelraces: [],
+    };
+  }
+
+  componentDidMount() {
+  reference.on('value', (snapshot) => {
+    const barrelraces = snapshot.val() || {};
+    this.setState({
+      barrelraces: map(barrelraces, (val, key) => extend(val, { key, name }))
+    });
+  });
+}
+
   render() {
     return (
       <div className="App">
