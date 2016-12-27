431038b8ef96183ab491569944c5cb1fcea26c32
Kristen Burgess <kristen.burgess@doane.edu>
2016-12-27 09:21:58 -0700

    Render all info for races, apply styling to title headers

--- src/DisplayEvent.js
+++ src/DisplayEvent.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
+import './DisplayEvent.css'
 
 class DisplayEvent extends Component {
   render() {
     const { event }  = this.props;
 
+
+
     return (
-      <div className="Event">
-        <div>
-          <h3>{event.city}</h3>
+      <div>
+        <div className="Event">
+          <h4> Event </h4>
+          <p> {event.title} </p>
+          <p> {event.date} </p>
+
+          <h4> Location </h4>
+          <p> {event.location} </p>
+          <p> {event.city}, {event.state}</p>
+
+        <h4> Details </h4>
+          <p> Sanction: {event.saction}</p>
+          <p> Added $$: {event.addedmoney} </p>
+          <p> Time: {event.time} </p>
+
+          <h4> Entries </h4>
+          <p>Fee/Horse: {event.entryfee} </p>
+          <p> Payout: {event.payout} </p>
+
+          <h4> Contact Info </h4>
+          <p> {event.contactName} </p>
+          <p> {event.contactNumber} </p>
+
+          <h4> Description </h4>
+          <p> {event.description}</p>
+
         </div>
       </div>
     );
