afeea914e08e010f5ea921be545e710aa68e1afa
Kristen Burgess <kristen.burgess@doane.edu>
2016-12-27 12:58:38 -0700

    Show only event name, date, location. Add'l info displays on click

--- src/DisplayEvent.js
+++ src/DisplayEvent.js
@@ -2,39 +2,56 @@ import React, { Component } from 'react';
 import './DisplayEvent.css'
 
 class DisplayEvent extends Component {
+  constructor() {
+  super()
+  this.state = {
+    hideDisplay: true,
+  }
+}
+
+toggleHideDisplay() {
+  this.setState({
+    hideDisplay: !this.state.hideDisplay
+  })
+}
+
   render() {
     const { event }  = this.props;
-
-
 
     return (
       <div>
         <div className="Event">
+        <section
+          onClick={this.toggleHideDisplay.bind(this)}>
           <h4> Event </h4>
-          <p> {event.title} </p>
-          <p> {event.date} </p>
-
+            <p> {event.title} </p>
+            <p> {event.date} </p>
           <h4> Location </h4>
-          <p> {event.location} </p>
-          <p> {event.city}, {event.state}</p>
+            <p> {event.location} </p>
+            <p> {event.city}, {event.state}</p>
+          </section>
 
+          <section
+            hidden={this.state.hideDisplay}
+            className='hiddeninfo'
+          >
         <h4> Details </h4>
           <p> Sanction: {event.saction}</p>
           <p> Added $$: {event.addedmoney} </p>
           <p> Time: {event.time} </p>
 
-          <h4> Entries </h4>
-          <p>Fee/Horse: {event.entryfee} </p>
+        <h4> Entries </h4>
+          <p>Fee/Horse: ${event.entryfee} </p>
           <p> Payout: {event.payout} </p>
 
-          <h4> Contact Info </h4>
+        <h4> Contact Info </h4>
           <p> {event.contactName} </p>
           <p> {event.contactNumber} </p>
 
           <h4> Description </h4>
           <p> {event.description}</p>
-
-        </div>
+      </section>
+      </div>
       </div>
     );
   }
