0d20c862453512c4f182964c4f5f3d5fdfeb0392
Kristen Burgess <kristen.burgess@doane.edu>
2016-12-26 18:55:35 -0700

    EventList component successfully hooked up to app

---
+++ src/EventList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class EventList extends Component {
+  render() {
+    return (
+      <div className="EventList">
+        <div>
+          <h2>BARREL RACES</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default EventList;
