f48571cbc7356357db032314932a20f68ec40820
Kristen Burgess <kristen.burgess@doane.edu>
2017-01-09 11:46:27 -0700

    Broke LocationDropdown in Filter into its own component

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../styles/Filter.css'
+import LocationDropdown from './LocationDropdown'
 
 class Filter extends Component {
   constructor() {
@@ -11,6 +12,17 @@ class Filter extends Component {
       state: '',
     }
   }
+
+  filterByState(selectedState) {
+    if(selectedState){
+      let filteredEvents = this.props.eventList.filter(event => {
+        return event.state.toUpperCase().match(selectedState)
+      })
+      this.props.filteredDisplay(filteredEvents);
+    } else {
+      this.props.filteredDisplay(null);
+      }
+    }
 
 filterByDate(e) {
   this.setState({date: e.target.value});
@@ -28,17 +40,6 @@ filterByDate(e) {
   }
 }
 
-filterByState(e) {
-  this.setState({state: e.target.value});
-  if (e.target.value) {
-  let filteredEvents = this.props.eventList.filter(event => {
-    return event.state.toUpperCase().match(e.target.value)
-  })
-  this.props.filteredDisplay(filteredEvents);
-} else {
-  this.props.filteredDisplay(null);
-  }
-}
 
 filterByMoney(e) {
   this.setState({addedmoney: e.target.value});
@@ -76,6 +77,8 @@ checkSanctions(sanctions) {
 
 
 render () {
+
+  const { eventList } = this.props
 
   return (
     <div className='filter-form'>
@@ -127,64 +130,10 @@ render () {
     onChange={(e)=> this.filterByMoney(e)}
     />
   <br></br>
-  <p className='filter-location'> State : </p>
-    <select
-      className='state-dropdown filter-location'
-      value={this.state.state}
-      onChange={(e)=> this.filterByState(e)}
-    >
-  	<option value="AL">Alabama</option>
-  	<option value="AK">Alaska</option>
-  	<option value="AZ">Arizona</option>
-  	<option value="AR">Arkansas</option>
-  	<option value="CA">California</option>
-  	<option value="CO">Colorado</option>
-  	<option value="CT">Connecticut</option>
-  	<option value="DE">Delaware</option>
-  	<option value="DC">District Of Columbia</option>
-  	<option value="FL">Florida</option>
-  	<option value="GA">Georgia</option>
-  	<option value="HI">Hawaii</option>
-  	<option value="ID">Idaho</option>
-  	<option value="IL">Illinois</option>
-  	<option value="IN">Indiana</option>
-  	<option value="IA">Iowa</option>
-  	<option value="KS">Kansas</option>
-  	<option value="KY">Kentucky</option>
-  	<option value="LA">Louisiana</option>
-  	<option value="ME">Maine</option>
-  	<option value="MD">Maryland</option>
-  	<option value="MA">Massachusetts</option>
-  	<option value="MI">Michigan</option>
-  	<option value="MN">Minnesota</option>
-  	<option value="MS">Mississippi</option>
-  	<option value="MO">Missouri</option>
-  	<option value="MT">Montana</option>
-  	<option value="NE">Nebraska</option>
-  	<option value="NV">Nevada</option>
-  	<option value="NH">New Hampshire</option>
-  	<option value="NJ">New Jersey</option>
-  	<option value="NM">New Mexico</option>
-  	<option value="NY">New York</option>
-  	<option value="NC">North Carolina</option>
-  	<option value="ND">North Dakota</option>
-  	<option value="OH">Ohio</option>
-  	<option value="OK">Oklahoma</option>
-  	<option value="OR">Oregon</option>
-  	<option value="PA">Pennsylvania</option>
-  	<option value="RI">Rhode Island</option>
-  	<option value="SC">South Carolina</option>
-  	<option value="SD">South Dakota</option>
-  	<option value="TN">Tennessee</option>
-  	<option value="TX">Texas</option>
-  	<option value="UT">Utah</option>
-  	<option value="VT">Vermont</option>
-  	<option value="VA">Virginia</option>
-  	<option value="WA">Washington</option>
-  	<option value="WV">West Virginia</option>
-  	<option value="WI">Wisconsin</option>
-  	<option value="WY">Wyoming</option>
-  </select>
+  <LocationDropdown
+    filterByState={this.filterByState.bind(this)}
+    eventlist={eventList}
+  />
     </div>
     )
   }
