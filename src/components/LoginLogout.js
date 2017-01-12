bd9e7360f1beee5db87919542686709fd99706f5
Kristen Burgess <kristen.burgess@doane.edu>
2017-01-12 01:24:41 -0700

    Set up GH pages

---
+++ src/components/LoginLogout.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import firebase, { signIn, signOut } from '../firebase'
+
+export const LoginLogout = ({ user }) => {
+  return (
+    <section className='login-status-bar'>
+      {user ?
+        <section className='user-login-info'>
+          <section className='logged-in-text'>
+            <h3 className='login-as-text'>Currently signed in as:  </h3>
+            <h3 className='user-name'>{user.displayName} </h3>
+          </section>
+
+          <button
+            className='logout-btn'
+            onClick={() => signOut()}>
+            Sign Out
+          </button>
+        </section> :
+        <button
+          className='login-btn'
+          onClick={() => signIn()}>
+          Sign In
+        </button>
+      }
+    </section>
+  )
+}
